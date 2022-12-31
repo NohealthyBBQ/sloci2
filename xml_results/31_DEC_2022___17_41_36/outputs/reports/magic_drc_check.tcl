d668e62c33f27c8d5280754a07f427b1727aeacc
NohealthyBBQ <zexil@andrew.cmu.edu>
2022-12-31 12:51:39 -0500

    precheck results

---
+++ xml_results/31_DEC_2022___17_41_36/outputs/reports/magic_drc_check.tcl
@@ -0,0 +1,4 @@
+box 1558.800um 3042.800um 1558.995um 3044.750; feedback add "MiM cap bottom plate spacing < 1.2um (capm.2b)" medium
+box 1560.000um 3044.000um 1560.195um 3045.500; feedback add "MiM cap bottom plate spacing < 1.2um (capm.2b)" medium
+box 1559.300um 3217.300um 1559.500um 3217.500; feedback add "MiM cap bottom plate spacing < 1.2um (capm.2b)" medium
+box 1560.500um 3218.500um 1560.700um 3218.700; feedback add "MiM cap bottom plate spacing < 1.2um (capm.2b)" medium
